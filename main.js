33159d1c936749b968876bec81cc6b5afd965b3e
Ada Nanic <ada.nanic91@gmail.com>
2023-02-05 21:33:17 +0100

    Splneny domaci ukol

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
 // tady je místo pro náš program
 
+let vysledek = document.querySelector('.vysledekVek');
+let celeJmeno = prompt('Jake je vase jmeno a prijmeni?');
+let rokNarozeni = Number(prompt('Jaky je vas rok narozeni?'));
+const d = new Date();
+let year = d.getFullYear();
+let vek = year - rokNarozeni;
+
+vysledek.innerHTML =  "<p>" + celeJmeno + " " + vek + "</p>";
+
+// funguje
+
+let barva = prompt('Jaka je vase oblibena barva? Vypiste anglicky:');
+vysledek.style.color = barva;
+
+// funguje
 
 
 
